1f9199d1229bd9bff50698d2c02c6cd8813c3c14
wailanbrea <45805381+wailanbrea@users.noreply.github.com>
2026-09-18 20:49:11 -0400

    build: separate debug and production API endpoints

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,13 +17,15 @@ android {
         targetSdk = 37
         versionCode = 1
         versionName = "1.0"
-        buildConfigField("String", "DEFAULT_API_BASE_URL", "\"http://192.168.100.159:8000/api/v1/\"")
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
+        debug {
+            buildConfigField("String", "DEFAULT_API_BASE_URL", "\"http://192.168.100.159:8000/api/v1/\"")
+        }
         release {
+            buildConfigField("String", "DEFAULT_API_BASE_URL", "\"https://amsport.api.bsolutions.dev/api/v1/\"")
             optimization {
                 enable = false
             }
